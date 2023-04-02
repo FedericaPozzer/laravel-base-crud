ea89c80ac5cbe6e4c7f0888960b639e8d9e49fdf
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-02 20:26:07 +0200

    added "route to the songs list" to my hompage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,15 @@
 
 @section("page-name", "homepage")
 
-@section("main-content", "contenuto della homepage")
+@section("main-content")
+
+    <div class="container">
+        <h2> 
+            <a href="{{ route('songs.index') }}">
+                Go to the songs list!
+            </a>
+        </h2>
+
+    </div>
+
+@endsection
