f6f21e002ca897ef5753119476efdcdd9e9cd903
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-04 17:47:09 +0200

    made the errors look prettier

--- resources/views/songs/edit.blade.php
+++ resources/views/songs/edit.blade.php
@@ -5,10 +5,11 @@
 @section("main-content")
 
     @if($errors->any())
-    <div class="alert alert-danger">
-        <ul>
+    <div class="alert alert-danger d-inline-block">
+        <ul class="m-0 ps-0">
             @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
+            {{-- <li>{{ $error }}</li> --}}
+                <div>You typed somethig wrong, please check below to find your error!</div>
             @endforeach
         </ul>
     </div>
@@ -24,7 +25,7 @@
 
         <div class="col-7 col-offset-5 my-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" @error("title") is-invalid @enderror id="title" name="title" value="{{ old("title") ?? $song->title }}">
+            <input type="text" class="form-control @error("title") is-invalid @enderror" id="title" name="title" value="{{ old("title") ?? $song->title }}">
             @error("title")
             <div class="invalid-feedback"> {{ $message }} </div>
             @enderror
@@ -32,7 +33,7 @@
 
         <div class="col-7 col-offset-5 my-3">
             <label for="album" class="form-label">Album</label>
-            <input type="text" class="form-control" @error("album") is-invalid @enderror id="album" name="album" value="{{ old("album") ?? $song->album }}">
+            <input type="text" class="form-control @error("album") is-invalid @enderror" id="album" name="album" value="{{ old("album") ?? $song->album }}">
             @error("album")
             <div class="invalid-feedback"> {{ $message }} </div>
             @enderror
@@ -40,7 +41,7 @@
 
         <div class="col-7 col-offset-5 my-3">
             <label for="author" class="form-label">Author</label>
-            <input type="text" class="form-control" @error("author") is-invalid @enderror id="author" name="author" value="{{ old("author") ?? $song->author }}">
+            <input type="text" class="form-control @error("author") is-invalid @enderror" id="author" name="author" value="{{ old("author") ?? $song->author }}">
             @error("author")
             <div class="invalid-feedback"> {{ $message }} </div>
             @enderror
@@ -48,7 +49,7 @@
 
         <div class="col-7 col-offset-5 my-3">
             <label for="editor" class="form-label">Editor</label>
-            <input type="text" class="form-control" @error("editor") is-invalid @enderror id="editor" name="editor" value="{{ old("editor") ?? $song->editor }}">
+            <input type="text" class="form-control @error("editor") is-invalid @enderror" id="editor" name="editor" value="{{ old("editor") ?? $song->editor }}">
             @error("editor")
             <div class="invalid-feedback"> {{ $message }} </div>
             @enderror
@@ -56,7 +57,7 @@
 
         <div class="col-7 col-offset-5 my-3">
             <label for="length" class="form-label">Length</label>
-            <input type="text" class="form-control" @error("length") is-invalid @enderror id="length" name="length" value="{{ old("length") ?? $song->length }}">
+            <input type="text" class="form-control @error("length") is-invalid @enderror" id="length" name="length" value="{{ old("length") ?? $song->length }}">
             @error("length")
             <div class="invalid-feedback"> {{ $message }} </div>
             @enderror
@@ -64,7 +65,7 @@
 
         <div class="col-7 col-offset-5 my-3">
             <label for="poster" class="form-label">poster</label>
-            <input type="text" class="form-control" @error("poster") is-invalid @enderror id="poster" name="poster" value="{{ old("poster") ?? $song->poster }}">
+            <input type="text" class="form-control @error("poster") is-invalid @enderror" id="poster" name="poster" value="{{ old("poster") ?? $song->poster }}">
             @error("poster")
             <div class="invalid-feedback"> {{ $message }} </div>
             @enderror
